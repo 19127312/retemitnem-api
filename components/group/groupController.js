e3c93d0a5d6122ca5cad1d7401172bfda79f6ceb
luong07734 <le.ky.luong@gmail.com>
2022-11-18 11:42:51 +0700

    group get info APIs

--- components/group/groupController.js
+++ components/group/groupController.js
@@ -1,15 +1,16 @@
 const groupService = require('./groupService');
+const userService = require("../user/userService");
 
 module.exports.createGroup = async (req, res) => {
     try {
-        const { groupName,creatorID, creatorEmail } = req.body;
+        const { groupName, creatorID } = req.body;
         // User input validation
         // ...
         // User input validation in frontend
 
         const existGroup = await groupService.findGroupByCreatorAndName(creatorID, groupName);
         if (!existGroup) {
-            await groupService.createGroup(groupName, creatorID, creatorEmail);
+            await groupService.createGroup(groupName, creatorID);
             res.status(200).send('success');
         } else {
             res.status(409).send('group name has already been used');
@@ -22,10 +23,42 @@ module.exports.createGroup = async (req, res) => {
 
 module.exports.viewListOfGroups = async (req, res) => {
     try {
-        const list = await groupService.findAll();
+        let list = await groupService.findAll();
         if (list) {
-            console.log(list);
-            res.status(200).json(list);
+            for(let i =0; i < list.length; i++){
+                let newMemberList = list[i].members.map(item => {
+                    return{
+                        _id: item._id,
+                        memberID : item.memberID,
+                        role : item.role
+                    }
+                })
+                for (let j = 0; j < newMemberList.length; j++) {
+                    const memberInfo = await userService.findUserInfo(newMemberList[j].memberID);
+                    newMemberList[j] = {...newMemberList[i], memberName: memberInfo.fullName, memberEmail: memberInfo.email }
+                }
+            }
+
+            console.log("list", list.members);
+
+            
+
+            let newList = list.map(item => {
+                return {
+                    _id: item._id,
+                    members: item.members,
+                    createdDate: item.createdDate,
+                    creatorID: item.creatorID,
+                    groupName: item.groupName
+                }
+            })
+
+
+            for (let i = 0; i < newList.length; i++) {
+                const userInfo = await userService.findUserInfo(newList[i].creatorID);
+                newList[i] = { ...newList[i], creatorName: userInfo.fullName, creatorEmail: userInfo.email };
+            }
+            res.status(200).json(newList);
         }
 
     } catch (e) {
