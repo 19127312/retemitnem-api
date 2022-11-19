6a6047fc7738ccbfa41ee653dc69738dad519c69
19127312 <63494302+19127312@users.noreply.github.com>
2022-11-19 11:38:34 +0700

    Register GG

--- components/auth/authService.js
+++ components/auth/authService.js
@@ -6,76 +6,97 @@ const axios = require("axios");
 const verifyToken = require("./verifyTokenModel");
 
 exports.validPassword = (password, user) => {
-    return bcrypt.compare(password, user.password);
+  return bcrypt.compare(password, user.password);
 };
 
 exports.findByUsername = (fullName) => {
-    return userModel
-        .findOne({
-            fullName,
-        })
-        .lean();
+  return userModel
+    .findOne({
+      fullName,
+    })
+    .lean();
 };
 
 exports.findByEmail = (email) => {
-    return userModel.findOne({ email }).lean();
+  return userModel.findOne({ email }).lean();
 };
 
 exports.findToken = (token) => {
-    return verifyToken.findOne({ token }).lean();
-}
+  return verifyToken.findOne({ token }).lean();
+};
+
+exports.deleteToken = (token) => {
+  return verifyToken.findOneAndDelete({ token }).lean();
+};
 
 exports.updateUser = (user) => {
-    userModel.findOneAndUpdate({ _id: user._id }, user, { new: true }, (err, doc) => {
-
-    })
-}
+  userModel.findOneAndUpdate(
+    { _id: user._id },
+    user,
+    { new: true },
+    (err, doc) => {}
+  );
+};
 
 exports.createAccessToken = ({ email, _id, fullName }) => {
-    return jwt.sign({
-        email,
-        _id,
-        fullName
-    }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '15m'
-    });
-}
+  return jwt.sign(
+    {
+      email,
+      _id,
+      fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "15m",
+    }
+  );
+};
 
 exports.createRefreshToken = ({ email, _id, fullName }, refreshTokenId) => {
-    return jwt.sign({
-        email,
-        _id,
-        fullName,
-        tokenId: refreshTokenId
-    }, process.env.REFRESH_TOKEN_SECRET, {
-        expiresIn: '30d'
-    });
-}
+  return jwt.sign(
+    {
+      email,
+      _id,
+      fullName,
+      tokenId: refreshTokenId,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
 
 exports.validateRefreshToken = async (token) => {
-    const decodeToken = () => {
-        try {
-            return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
-        } catch (err) {
-            throw new Error("Token is invalid");;
-        }
+  const decodeToken = () => {
+    try {
+      return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+    } catch (err) {
+      throw new Error("Token is invalid");
     }
+  };
 
-    const decodedToken = decodeToken();
+  const decodedToken = decodeToken();
 
-    const tokenExists = await refreshTokenModel.exists({ _id: decodedToken.tokenId, owner: decodedToken._id });
-    if (tokenExists) {
-        return decodedToken;
-    } else {
-        throw new Error("Token is not in databse");
-    }
+  const tokenExists = await refreshTokenModel.exists({
+    _id: decodedToken.tokenId,
+    owner: decodedToken._id,
+  });
+  if (tokenExists) {
+    return decodedToken;
+  } else {
+    throw new Error("Token is not in databse");
+  }
 };
 
 exports.getGoogleUserInfo = async (token) => {
-    const response = await axios.get("https://www.googleapis.com/oauth2/v2/userinfo", {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
-    return response.data;
-}
+  const response = await axios.get(
+    "https://www.googleapis.com/oauth2/v2/userinfo",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return response.data;
+};
--- components/auth/index.js
+++ components/auth/index.js
@@ -10,6 +10,6 @@ router.post("/access-token", authController.createNewAccessToken);
 router.post("/logout", authController.logout);
 router.post("/logout-all", authController.logoutAll);
 router.post("/google_login", authController.googleLogin);
-router.post("/google_register", authController.googleRegister);
+router.post("/google_signup", authController.googleRegister);
 router.get("/confirmation/:email/:token", authController.confirmation);
 module.exports = router;
