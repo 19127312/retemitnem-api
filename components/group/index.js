9e1525383b0ef2d9c31f3df70b8019dac4770329
19127324 <duyan0612@gmail.com>
2022-11-22 13:43:28 +0700

    send link to email api

--- components/group/index.js
+++ components/group/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var express = require("express");
 const router = express.Router();
-const groupController = require('./groupController');
+const groupController = require("./groupController");
 const jwtMiddleware = require("../../passport/jwtMiddleware");
 
-router.post('/add', jwtMiddleware, groupController.createGroup);
-router.get('/info', jwtMiddleware, groupController.viewListOfGroups);
-router.post('/role', groupController.changeRole);
-router.post('/deletemember', groupController.kickMember);
-router.post('/addmember', groupController.addMember);
+router.post("/add", jwtMiddleware, groupController.createGroup);
+router.get("/info", jwtMiddleware, groupController.viewListOfGroups);
+router.post("/role", groupController.changeRole);
+router.post("/deletemember", groupController.kickMember);
+router.post("/addmember", groupController.addMember);
+router.post("/sendlinktoemail", groupController.sendLinkToEmail);
 module.exports = router;
