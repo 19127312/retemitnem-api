a8bfbc53a0226e65fdf001c1ce3d2e56f2c4c5c7
19127312 <63494302+19127312@users.noreply.github.com>
2022-12-01 17:33:16 +0700

    Add realtime submit

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const userRouter = require("./components/user");
 const groupRouter = require("./components/group");
 const presentationRouter = require("./components/presentation");
 const app = express();
-const dotenv = require("dotenv").config();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
